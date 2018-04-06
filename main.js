806a794a1809aaa067c11bb865bcc5155dce4696
Omar Curiel <omarcuriel@Omars-MacBook-Pro.local>
2018-04-06 11:06:43 -0700

    RANDOM COLORS

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
-let colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-]
+let colors = genColors(6);
 
 let squares = document.querySelectorAll('.square');
 let colorDisplay = document.getElementById('colorDisplay');
 let msg = document.querySelector('#msg');
+let header = document.querySelector('h1');
 let pickedColor = pickColor();
 
 colorDisplay.textContent = pickedColor;
@@ -21,21 +15,37 @@ for (var i = 0; i < colors.length; i++) {
     if (clickedColor === pickedColor) {
       msg.textContent = 'Correct!';
       changeColor(clickedColor);
+      header.style.backgroundColor = pickedColor;
     } else {
       this.style.backgroundColor = '#232323';
       msg.textContent = 'Try Again!';
     }
   });
-}
+};
 
 function changeColor (color) {
   for (let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = color;
-  }
-}
+  };
+};
 
 function pickColor () {
   //pick random number
   let random = Math.floor(Math.random() * colors.length);
   return colors[random];
-}
+};
+
+function genColors (num) {
+  let arr = [];
+  for (let i = 0; i < num; i++) {
+    arr.push(randomColor());
+  };
+  return arr;
+};
+
+function randomColor () {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+};
